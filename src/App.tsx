813b83cac15bb3af7fcced57c0e9f1f2d3ea8e60
Haixiang <haixiang6123@gmail.com>
2020-04-28 13:25:54 -0700

    封装 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,51 @@
 import React from 'react'
 import {HashRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
-import styled from 'styled-components'
-import Nav from 'components/Nav'
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`
+import Layout from './components/Layout'
 
 export default function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags/>
-            </Route>
-            <Route path="/money">
-              <Money/>
-            </Route>
-            <Route path="/statistics">
-              <Statistics/>
-            </Route>
-            <Redirect exact from="/" to="/money"/>
-            <Route>
-              <NotMatch/>
-            </Route>
-          </Switch>
-        </Main>
-
-        <Nav/>
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route>
+          <NotMatch/>
+        </Route>
+      </Switch>
     </Router>
   )
 }
 
 function Tags() {
-  return <h2>标签</h2>
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  )
 }
 
 function Money() {
-  return <h2>记账</h2>
+  return (
+    <Layout>
+      <h2>记账页面</h2>
+    </Layout>
+  )
 }
 
 function Statistics() {
-  return <h2>统计</h2>
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  )
 }
 
 function NotMatch() {
---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import styled from 'styled-components'
+import Nav from './Nav'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+
+const Layout: React.FunctionComponent = (props: any) => {
+  return (
+    <Wrapper>
+      <Main>
+        {props.children}
+      </Main>
+      <Nav/>
+    </Wrapper>
+  )
+}
+
+export default Layout
