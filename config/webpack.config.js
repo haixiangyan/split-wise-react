b415238cf07f832d1ee4c2fcfc7f1a884eb01ca7
Haixiang <haixiang6123@gmail.com>
2020-04-28 12:07:04 -0700

    引入 Icon

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -348,6 +348,13 @@ module.exports = function(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            {
+              test: /\.svg$/,
+              use: [
+                {loader: 'svg-sprite-loader', options: {}},
+                {loader: 'svgo-loader', options: {}}
+              ]
+            },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,9 +8,17 @@ const NavWrapper = styled.nav`
   > ul {
     display: flex;
     > li {
-       padding: 16px;
+       padding: 4px 0;
        width: 33.333333%;
        text-align: center;
+       display: flex;
+       flex-direction: column;
+       align-items: center;
+       font-size: .9em;
+       .icon {
+         width: 24px;
+         height: 24px;
+       }
     }
   }
 `
@@ -20,12 +28,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-tag"/>
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-money"/>
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon" aria-hidden="true">
+            <use xlinkHref="#icon-chart"/>
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
