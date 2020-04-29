312c3e9deedd965e79468b4e57adf46b763acf25
Haixiang <haixiang6123@gmail.com>
2020-04-29 16:59:47 -0700

    完成 NumberPad 的监听

--- src/pages/Money/NumberPadSection.tsx
+++ src/pages/Money/NumberPadSection.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 import * as React from 'react'
+import {useState} from 'react'
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   .output {
@@ -49,13 +50,46 @@ const NumberPadSection = styled.section`
   }
 `
 
-export default () => {
+const NumberPadSection: React.FC = () => {
+  const [output, setOutput] = useState('0')
+
+  const onClickPad = (e: React.MouseEvent<HTMLDivElement>) => {
+    const text = (e.target as HTMLButtonElement).textContent
+    if (text === null) return
+
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '.':
+        if (output === '0') {
+          setOutput(text)
+        } else {
+          setOutput(output + text)
+        }
+        break
+      case '删除':
+        break
+      case '清空':
+        break
+      case 'OK':
+        break
+    }
+  }
+
   return (
-    <NumberPadSection>
+    <Wrapper>
       <div className="output">
-        100
+        {output}
       </div>
-      <div className="pad clearfix">
+      <div className="pad clearfix" onClick={onClickPad}>
         <button>1</button>
         <button>2</button>
         <button>3</button>
@@ -74,6 +108,8 @@ export default () => {
         <button className="zero">0</button>
         <button className="dot">.</button>
       </div>
-    </NumberPadSection>
+    </Wrapper>
   )
 }
+
+export default NumberPadSection
