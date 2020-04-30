dbe0d4c33a15f6c7b285dbac1c443288ee4cfc2a
Haixiang <haixiang6123@gmail.com>
2020-04-29 19:04:10 -0700

    完成 TagList 样式

--- src/pages/Tags/index.tsx
+++ src/pages/Tags/index.tsx
@@ -1,13 +1,60 @@
 import * as React from 'react'
 import Layout from '../../components/Layout'
 import useTags from '../../useTags'
+import styled from 'styled-components'
+import Icon from '../../components/Icon'
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: #fff;
+  > li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    border-bottom: 1px solid #e5e5e5;
+    line-height: 20px;
+  }
+`
+
+const AddTagButton = styled.button`
+  padding: 8px 12px;
+  font-size: 18px;
+  border: none;
+  border-radius: 4px;
+  background: #f60;
+  color: #fff;
+`
+
+const Center = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const Space = styled.div`
+  height: 16px;
+`
 
 const Tags: React.FunctionComponent = () => {
   const {tags, setTags} = useTags()
 
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <span className="one-line">{tag}</span>
+            <Icon name="right"/>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Space/>
+        <AddTagButton>新增标签</AddTagButton>
+      </Center>
     </Layout>
   )
 }
