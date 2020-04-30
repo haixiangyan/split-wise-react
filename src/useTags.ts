af50c703d437e5c8fa8dbda6ccfddda1a4565e8e
Haixiang <haixiang6123@gmail.com>
2020-04-30 00:25:35 -0700

    使用默认的 tag list

--- src/useTags.ts
+++ src/useTags.ts
@@ -6,13 +6,15 @@ export type TTag = {
   name: string
 }
 
+const defaultTags = [
+  {id: createId(), name: '衣'},
+  {id: createId(), name: '食'},
+  {id: createId(), name: '住'},
+  {id: createId(), name: '行'},
+]
+
 const useTags = () => {
-  const [tags, setTags] = useState<TTag[]>([
-    {id: createId(), name: '衣'},
-    {id: createId(), name: '食'},
-    {id: createId(), name: '住'},
-    {id: createId(), name: '行'},
-  ])
+  const [tags, setTags] = useState<TTag[]>(defaultTags)
   return {tags, setTags}
 }
 
