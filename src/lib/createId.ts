53630a148e785163d9d5d0b69560d0f5fe051f29
Haixiang <haixiang6123@gmail.com>
2020-05-14 13:16:09 -0700

    tags 数据持久化

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,7 +1,8 @@
-let id = 0
+let id = parseInt(window.localStorage.getItem('idMax') || '0')
 
 const createId = () => {
   id += 1
+  window.localStorage.setItem('idMax', id.toString())
   return id
 }
 
--- src/useTags.ts
+++ src/useTags.ts
@@ -7,26 +7,28 @@ export type TTag = {
   name: string
 }
 
-const defaultTags = [
-  {id: createId(), name: '衣'},
-  {id: createId(), name: '食'},
-  {id: createId(), name: '住'},
-  {id: createId(), name: '行'},
-]
-
 const useTags = () => {
   const [tags, setTags] = useState<TTag[]>([])
 
   useEffect(() => {
-    setTags(JSON.parse(window.localStorage.getItem('tags') || '[]'))
-  }, []) // 第一次渲染
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]')
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: '衣'},
+        {id: createId(), name: '食'},
+        {id: createId(), name: '住'},
+        {id: createId(), name: '行'},
+      ]
+    }
+    setTags(localTags)
+  }, []) // 第一次渲染，组件挂载时执行
   useUpdate(() => {
     window.localStorage.setItem('tags', JSON.stringify(tags))
   }, [tags]) // 监听 tags
 
   const findTag = (id: number) => tags.find(t => t.id === id)
 
-  const updateTags = (id: number, {name}: {name: string}) => {
+  const updateTags = (id: number, {name}: { name: string }) => {
     setTags(tags.map(tag => tag.id === id ? {id, name} : tag))
   }
 
@@ -36,7 +38,7 @@ const useTags = () => {
 
   const addTag = () => {
     const tagName = window.prompt('新标签的名称为')
-    if (tagName !== null) {
+    if (tagName !== null && tagName !== '') {
       setTags([...tags, {
         id: createId(),
         name: tagName
