3650d18473588efe375372b7e65d5c49d3706656
Haixiang <haixiang6123@gmail.com>
2020-05-14 22:55:27 -0700

    Updates

---
+++ precache-manifest.d5bcda28824ca3fbd03456d509cee64d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ee56b6c0b4bb65a8f3f2ca796a2548",
+    "url": "/split-wise-react/index.html"
+  },
+  {
+    "revision": "d18d582d2ba2dcaf124c",
+    "url": "/split-wise-react/static/css/main.74747607.chunk.css"
+  },
+  {
+    "revision": "fd871d768702915664bc",
+    "url": "/split-wise-react/static/js/2.6d109f58.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/split-wise-react/static/js/2.6d109f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18d582d2ba2dcaf124c",
+    "url": "/split-wise-react/static/js/main.97eff37e.chunk.js"
+  },
+  {
+    "revision": "d3007962efb55daf270e",
+    "url": "/split-wise-react/static/js/runtime-main.98080fcf.js"
+  }
+]);
